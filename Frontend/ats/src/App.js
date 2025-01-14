06afcc299bf34dc050a2b0ea0c7f9519bd042d24
Sukriti Chawla <sukriti.gmail.com>
2025-01-14 19:47:51 +0530

    merge

--- Frontend/ats/src/App.js
+++ Frontend/ats/src/App.js
@@ -6,12 +6,8 @@ function App() {
       <h1>
         ATS
       </h1>
-<<<<<<< HEAD
       <p>sukriti</p>
       <p>riyanshi</p>
-=======
-      <p>sukriti,n</p>
->>>>>>> 3290f7f91cb5526715fb0bd704080e8fa7921bd3
       <WebsiteLandingPage/>
     </>
   );
